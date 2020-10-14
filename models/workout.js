7b5908697cddbc8676a62b5e1b9b967140393288
Aram2142 <andresaaramirez@aol.com>
2020-10-13 20:12:18 -0400

    made mongoose database

---
+++ models/workout.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "enter type of workout"
+            },
+
+            name: {
+                type: String,
+                trim: true,
+                required: "enter a name of workout"
+            },
+
+            duration: {
+                type: Number
+            },
+
+            weight: {
+                type: Number
+            },
+
+            reps: {
+                type: Number
+            },
+
+            sets: {
+                type:Number
+            }
+        }
+    ]
+})
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
