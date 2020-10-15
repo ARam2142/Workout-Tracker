fc23ce89c617bb178f830f9c6262f1933944396b
Aram2142 <andresaaramirez@aol.com>
2020-10-14 23:33:39 -0400

    fixed handle routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,33 +1,16 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
-var path = require("path");
+const router = require("express").Router();
+const path = require("path");
 
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+router.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
-module.exports = function(app) {
-// HTML GET Requests
-// Below code handles when users "visit" a page.
-// In each of the below cases the user is shown an HTML page of content
-// ---------------------------------------------------------------------------
-    // If no matching route is found default to index
-    app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-        //res.render("../public/index.html")
-    });
+router.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-    app.get("/exercise", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-        //res.render("../public/exercise.html")
-    });
-
-    app.get("/stats", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
-        //res.render("../public/stats.html")
-    });
-
-};
+module.exports = router;
