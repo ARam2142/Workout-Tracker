16663a8d798de36ce21e6a48b933babe5e3ebc9d
Aram2142 <andresaaramirez@aol.com>
2020-10-18 14:01:10 -0400

    deploying

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
