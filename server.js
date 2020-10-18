aff643ecd4049857dfddcf77b2b8cfd7e9949f44
Aram2142 <andresaaramirez@aol.com>
2020-10-17 22:52:42 -0400

    changed to  logger in server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-const morgan = require("morgan");
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
-app.use(morgan("dev"));
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -13,16 +13,14 @@ app.use(express.static("public"));
 
 //connection to mongoosedb
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api-routes"));
 app.use(require("./routes/html-routes"));
-//app.use(htmlRouter);
-//app.use(apiRouter);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
